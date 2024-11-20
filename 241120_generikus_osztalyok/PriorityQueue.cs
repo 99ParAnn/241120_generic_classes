b15c8dfbba200a2b6c9843d18b92e58259930fb2
Petrik Diak <diak@petrik.hu>
2024-11-20 09:33:14 +0100

    Add project files.

---
+++ 241120_generikus_osztalyok/PriorityQueue.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _241120_generikus_osztalyok
+{
+	//TBA: sorted list so we can search logarythmically?
+	internal class PriorityQueue<T>
+	{
+		private List<KeyValuePair<T,int>> mainQueue;
+
+		public PriorityQueue()
+		{
+			this.mainQueue = new List<KeyValuePair<T, int>>();
+		}
+
+		public void Enqueue(T item, int priority)
+		{
+			this.mainQueue.Add(new KeyValuePair<T, int>(item,priority));
+		}
+
+		public T Dequeue() 
+		{
+			if (mainQueue.Count == 0)
+			{
+				throw new Exception("Tried to dequeue from an empty queue");
+			}
+			else
+			{
+				KeyValuePair<T, int> min = mainQueue[0];
+				foreach (var item in mainQueue)
+				{
+					if (min.Value > item.Value)
+					{
+						min = item;
+					}
+				}
+				mainQueue.Remove(min);
+				return min.Key;
+			}
+		}
+
+		public T Peek() 
+		{
+			if (mainQueue.Count == 0)
+			{
+				throw new Exception("Tried to peek at empty queue");
+				
+			}
+			else
+			{
+				KeyValuePair<T, int> min= mainQueue[0];
+				foreach (var item in mainQueue)
+				{
+					if (min.Value > item.Value)
+					{
+						min	= item; 
+					}
+				}
+				return min.Key;
+			}
+
+
+		}
+	}
+}
---
+++ 241120_generikus_osztalyok/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _241120_generikus_osztalyok
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			PriorityQueue<string> queue = new PriorityQueue<string>();
+			queue.Enqueue("a", 1);
+			queue.Enqueue("b", 2);
+			queue.Enqueue("c", 13);
+			queue.Enqueue("d", 1);
+
+			Console.Write(queue.Dequeue());
+            Console.WriteLine(queue.Peek());
+            
+		}
+	}
+}
